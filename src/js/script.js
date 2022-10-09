713a98f17e0f3f989833ce972568195adaa880f5
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-10-09 18:14:47 +0300

    in the process;)

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,44 @@
+//слайдер с акциями
+$(document).ready(function () {
+   $(".shares__cards-slider").slick({
+      arrows: true,
+      dots: false,
+      slidesToShow: 6,
+      speed: 1000,
+      autoplay: true,
+      slidesToScroll: 1,
+      infinite: false,
+      responsive: [{
+         breakpoint: 1280,
+         settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+         },
+      },
+      {
+         breakpoint: 1024,
+         settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+         },
+      },
+      {
+         breakpoint: 768,
+         settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+         },
+      },
+
+      ],
+   });
+});
+
+
+
+
+
+
 //открытие и закрытие спойлеров
 const spoilersNode = document.querySelectorAll('.spoiler')
 const spoilersArrowsNode = document.querySelectorAll('.spoiler__arrow')
@@ -39,43 +80,11 @@ sharesItemNode.forEach(elem => {
       this.classList.add('shares__nav-item-link--js-add-color')
    })
 })
+// выбор продукта чекбоксы
+const productsListItemNode = document.querySelectorAll('.products__list-item')
+productsListItemNode.forEach(el => {
+   el.addEventListener('click', function () {
+      el.classList.toggle('products__list-item--js-active')
 
-//слайдер с акциями
-$(document).ready(function () {
-   $(".shares__cards-slider").slick({
-      arrows: true,
-      dots: false,
-      slidesToShow: 6,
-      speed: 1000,
-      autoplay: true,
-      slidesToScroll: 1,
-      infinite: false,
-      responsive: [{
-         breakpoint: 1280,
-         settings: {
-            slidesToShow: 5,
-            slidesToScroll: 1,
-         },
-      },
-      {
-         breakpoint: 1024,
-         settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-         },
-      },
-      {
-         breakpoint: 768,
-         settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-         },
-      },
-
-      ],
-   });
-});
-
-
-
-
+   })
+})
