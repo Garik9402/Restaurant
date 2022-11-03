eb0689a7bfde9a2254b4b3edc775bb12e190fee7
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-11-03 18:50:10 +0300

    in the process;)

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,38 @@
-//слайдер с акциями
-$(document).ready(function () {
-   $(".shares__cards-slider").slick({
-      arrows: true,
-      dots: false,
-      slidesToShow: 6,
-      speed: 1000,
-      autoplay: true,
-      slidesToScroll: 1,
-      infinite: false,
-      responsive: [{
-         breakpoint: 1280,
-         settings: {
-            slidesToShow: 5,
-            slidesToScroll: 1,
-         },
-      },
-      {
-         breakpoint: 1024,
-         settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-         },
-      },
-      {
-         breakpoint: 768,
-         settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-         },
-      },
+// //слайдер с акциями
+// $(document).ready(function () {
+//    $(".shares__cards-slider").slick({
+//       arrows: true,
+//       dots: false,
+//       slidesToShow: 6,
+//       speed: 1000,
+//       autoplay: true,
+//       slidesToScroll: 1,
+//       infinite: false,
+//       responsive: [{
+//          breakpoint: 1280,
+//          settings: {
+//             slidesToShow: 5,
+//             slidesToScroll: 1,
+//          },
+//       },
+//       {
+//          breakpoint: 1024,
+//          settings: {
+//             slidesToShow: 4,
+//             slidesToScroll: 1,
+//          },
+//       },
+//       {
+//          breakpoint: 768,
+//          settings: {
+//             slidesToShow: 3,
+//             slidesToScroll: 1,
+//          },
+//       },
 
-      ],
-   });
-});
+//       ],
+//    });
+// });
 
 
 
@@ -205,17 +205,17 @@ additionalProduct.forEach(function (e) {
    })
 })
 // открытие инфо о продукте
-const dropdownInfo = document.querySelector('.popup-product__info')
-window.addEventListener('click', function (event) {
-   if (dropdownInfo.contains(event.target)) {
-      dropdownInfo.classList.toggle('popup-product__info--js-toggle')
-   }
-   else {
-      dropdownInfo.classList.remove('popup-product__info--js-toggle')
-   }
+const dropdownInfo = document.querySelectorAll('.popup-product__info')
+dropdownInfo.forEach(el => {
+   el.addEventListener('click', function () {
+      dropdownInfo.forEach(e => {
+         e.classList.toggle('popup-product__info--js-toggle')
+      })
+   })
 })
 
-// попап продукты 
+
+// попап открытие и закрытие продукта 
 const productNode = document.querySelectorAll('.product')
 const popupProductNode = document.querySelectorAll('.popup-product')
 productNode.forEach(el => {
@@ -227,7 +227,18 @@ productNode.forEach(el => {
       popupProductNode[index].classList.add('popup-product--js-open')
    })
 })
+const productCloseNode = document.querySelectorAll('.popup-product__close')
+window.addEventListener('click', function (event) {
+   productCloseNode.forEach(el => {
+      if (el.contains(event.target)) {
+         popupProductNode.forEach(el => {
+            el.classList.remove('popup-product--js-open')
+         })
+      }
+   })
 
+
+})
 
 
 
