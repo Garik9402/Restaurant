50a9be067e12066e2f1219cd55704cdda57891cb
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-11-05 18:05:41 +0300

    in the process;)

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,38 @@
-// //слайдер с акциями
-// $(document).ready(function () {
-//    $(".shares__cards-slider").slick({
-//       arrows: true,
-//       dots: false,
-//       slidesToShow: 6,
-//       speed: 1000,
-//       autoplay: true,
-//       slidesToScroll: 1,
-//       infinite: false,
-//       responsive: [{
-//          breakpoint: 1280,
-//          settings: {
-//             slidesToShow: 5,
-//             slidesToScroll: 1,
-//          },
-//       },
-//       {
-//          breakpoint: 1024,
-//          settings: {
-//             slidesToShow: 4,
-//             slidesToScroll: 1,
-//          },
-//       },
-//       {
-//          breakpoint: 768,
-//          settings: {
-//             slidesToShow: 3,
-//             slidesToScroll: 1,
-//          },
-//       },
+//слайдер с акциями
+$(document).ready(function () {
+   $(".shares__cards-slider").slick({
+      arrows: true,
+      dots: false,
+      slidesToShow: 6,
+      speed: 1000,
+      autoplay: true,
+      slidesToScroll: 1,
+      infinite: false,
+      responsive: [{
+         breakpoint: 1280,
+         settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+         },
+      },
+      {
+         breakpoint: 1024,
+         settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+         },
+      },
+      {
+         breakpoint: 768,
+         settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+         },
+      },
 
-//       ],
-//    });
-// });
+      ],
+   });
+});
 
 
 
@@ -47,13 +47,9 @@ const spoilerList = document.querySelectorAll('.sloiler__list')
 spoilersNode.forEach(elem => {
    elem.addEventListener('click', function () {
       spoilersNode.forEach(e => {
-         if (e.classList.contains('.spoiler--js-open-list')) {
-            e.classList.remove('spoiler--js-open-list')
-         }
-         this.classList.add('spoiler--js-open-list')
-
-
+         e.classList.remove('spoiler--js-open-list')
       })
+      this.classList.add('spoiler--js-open-list')
    })
 })
 
